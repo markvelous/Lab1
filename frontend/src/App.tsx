601f069ff81fda74fdf491368212f02a36a6aba1
markvelous <85272691+markvelous@users.noreply.github.com>
2021-08-12 15:42:21 +0800

    step 4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Symfoni } from './hardhat/SymfoniContext';
+import { Symfoni } from "./hardhat/SymfoniContext";
 import { Garage } from './components/Garage';
 
 function App() {
@@ -10,18 +10,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Symfoni autoInit={true} >
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
           <Garage></Garage>
         </Symfoni>
       </header>
